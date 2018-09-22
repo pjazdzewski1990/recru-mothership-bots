a4c2caefa0b9d192cbcc734ac3e2bce714f56b3c
Patryk <pjazdzewski1990@gmail.com>
2018-09-22 18:40:29 +0200

    remove Future.filter completely

--- src/main/scala/io/scalac/recru/HttpComms.scala
+++ src/main/scala/io/scalac/recru/HttpComms.scala
@@ -3,6 +3,7 @@ package io.scalac.recru
 import akka.NotUsed
 import com.typesafe.scalalogging.Logger
 import akka.stream.Materializer
+import play.api.libs.ws.StandaloneWSRequest
 import play.api.libs.ws.ahc.StandaloneAhcWSClient
 import spray.json.{JsNumber, JsObject, JsString, JsonParser}
 
@@ -32,29 +33,36 @@ class PlayHttpComms(baseUrl: String)
     val url = baseUrl + "game/"
     val b = JsObject("name" -> JsString(nameToUse)).prettyPrint
 
-    wsClient.url(url).addHttpHeaders("Content-Type" -> "application/json").post(b).filter(_.status == 200).flatMap { r =>
-      JsonParser(r.body) match {
-        case JsObject(fields) =>
-          val connectedOpt = for {
-            gameId <- fields.get("gameId").flatMap{
-              case JsString(v) => Some(GameId(v))
-              case _ => None
-            }
-            color <- fields.get("secretColor").flatMap{
-              case JsString(v) => Color.withNameOpt(v)
-              case _ => None
-            }.headOption
-            listen <- r.header("x-listen-on")
-          } yield {
-            ConnectedToGame(game = gameId, color = color, listenOn = listen)
-          }
+    wsClient.url(url).addHttpHeaders("Content-Type" -> "application/json").post(b).flatMap {
+      case r if r.status == 200 =>
+        parseConnectReponse(r)
+      case r =>
+        Future.failed(new IllegalArgumentException(s"Couldn't connect to game ${r.status}: ${r.body}"))
+    }
+  }
 
-          connectedOpt.map(Future.successful).getOrElse(
-            Future.failed(new IllegalArgumentException(s"Not enough data in ${fields}"))
-          )
-        case _ =>
-          Future.failed(new Exception("Cannot parse " + r.body))
-      }
+  private def parseConnectReponse(r: StandaloneWSRequest#Self#Response) = {
+    JsonParser(r.body) match {
+      case JsObject(fields) =>
+        val connectedOpt = for {
+          gameId <- fields.get("gameId").flatMap {
+            case JsString(v) => Some(GameId(v))
+            case _ => None
+          }
+          color <- fields.get("secretColor").flatMap {
+            case JsString(v) => Color.withNameOpt(v)
+            case _ => None
+          }.headOption
+          listen <- r.header("x-listen-on")
+        } yield {
+          ConnectedToGame(game = gameId, color = color, listenOn = listen)
+        }
+
+        connectedOpt.map(Future.successful).getOrElse(
+          Future.failed(new IllegalArgumentException(s"Not enough data in ${fields}"))
+        )
+      case _ =>
+        Future.failed(new Exception("Cannot parse " + r.body))
     }
   }
 
@@ -69,8 +77,13 @@ class PlayHttpComms(baseUrl: String)
     val post = wsClient.url(url)
       .addHttpHeaders("Content-Type" -> "application/json")
       .put(b)
-      .filter(_.status == 200)
-      .map(_ => NotUsed)
+      .flatMap {
+        case r if r.status == 200 =>
+          Future.successful(NotUsed)
+        case r =>
+          Future.failed(new IllegalArgumentException(s"Couldn't make a move ${r.status}: ${r.body}"))
+      }
+
     post.recover {
       case NonFatal(err) =>
         log.error(s"Was not able to send the move ${err.getMessage}", err)
